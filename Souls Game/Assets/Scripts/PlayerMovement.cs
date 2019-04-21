0200f93e17b1497f41876927c188b3764ff5eb2b
Jserna20 <31867660+Jserna20@users.noreply.github.com>
2019-04-21 00:02:47 -0400

    Fixed Loading Invalid bug

---
+++ Souls Game/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerMovement : MonoBehaviour 
+{
+    public CharacterController2D controller;
+    public Animator animator;
+    public int scene;
+    public float transDelay = 0.5f;
+    public float runSpeed = 40f;
+
+    float horizontalMove = 0f;
+    float verticalMove = 0f;
+    public int randomNum;
+    public int numForBattle = 5;
+    bool loaded = false;
+
+	private void Awake()
+	{
+        if(PlayerStats.PlayerPos != Vector3.zero)
+        {
+            transform.position = PlayerStats.PlayerPos;
+        }
+	}
+
+	// Update is called once per frame
+	void Update () 
+    {
+
+        if(!Menu.inMenuMode)
+        {
+            horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+            verticalMove = Input.GetAxisRaw("Vertical") * runSpeed;
+
+            animator.SetFloat("HorSpeed", Mathf.Abs(horizontalMove));
+            animator.SetFloat("VerSpeed", verticalMove);
+
+        }
+        else
+        {
+            animator.SetFloat("HorSpeed", 0f);
+            animator.SetFloat("VerSpeed", 0f);
+            horizontalMove = 0;
+            verticalMove = 0;
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            BeginBattle();
+        }
+
+        PlayerStats.PlayerPos = transform.position;
+
+	}
+
+	private void FixedUpdate()
+	{
+        
+            controller.MoveHorizontal(horizontalMove * Time.fixedDeltaTime);
+            controller.MoveVertical(verticalMove * Time.fixedDeltaTime);
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        
+        if (other.gameObject.CompareTag("BattleTrigger"))
+        {
+            randomNum = Random.Range(0, 11);
+            if (randomNum == numForBattle)
+            {
+                BeginBattle();
+            }
+        }
+    }
+
+    void DelayedBattle(float delay)
+    {
+        Invoke("BeginBattle", delay);
+    }
+
+    void BeginBattle()
+    {
+        horizontalMove = 0;
+        verticalMove = 0;
+        controller.MoveHorizontal(horizontalMove * Time.fixedDeltaTime);
+        controller.MoveVertical(verticalMove * Time.fixedDeltaTime);
+        PlayerStats.InBattle = true;
+        Menu.inBattle = true;
+        SceneManager.LoadSceneAsync(scene, LoadSceneMode.Additive);
+        loaded = true;
+    }
+}
