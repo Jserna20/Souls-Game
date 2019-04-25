0d0db1214899c32611af87e14e37d91b30efdaf4
Chad Pearson <G00254194@stu.raritanval.edu>
2019-04-25 10:52:41 -0400

    Added game over screen

---
+++ Souls Game/Assets/Scripts/GameOver.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour
+{
+    public void ReloadGame()
+    {
+        // Only specifying the sceneName or sceneBuildIndex will load the Scene with the Single mode
+        SceneManager.LoadScene("AlwaysHere");
+    }
+}
